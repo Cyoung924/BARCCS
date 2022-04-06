0d822e275288e3547ba82bf82e5706473408ceb0
Cyoung924 <101139483+Cyoung924@users.noreply.github.com>
2022-04-06 06:18:48 +0000

    V1.2 - Began basic code structure

--- CollarComms/Main.ino
+++ CollarComms/Main.ino
@@ -1,22 +1,49 @@
 // Libraries
 #include <SoftwareSerial.h>
+#include <Chrono.h>
 
-//Struct to hold UIK packet
+//Pin 3 on Arduino receives, pin 2 transmits
+#define RX 3
+#define TX 2
+#define BAUDRATE 9600
+
+//Defines BLE object as BTSerial
+SoftwareSerial BTSerial (RX,TX);
+
+Chrono chrono;
+
+//Struct to hold UIK packet. Dependent on React Native
+//reading and writing structure
+struct Packet {
+  int a;
+  int b;
+  int c;
+  int d;
+} pkt_tx, pkt_rx;
 
 //
 void setup() {
   //Inititalize baud rates for both serial and HM-10
+  Serial.begin(BAUDRATE);
+  BTSerial.begin(BAUDRATE);
   
   //Initialize w/ AT commands to complete before phone pairs
     //AT commands to query HM-10 device info for phone
     //AT commands to set info (BARCCS name, power, etc.)
+  BTSerial.println("AT+BAUD0")
+  BTSerial.println("AT+POWE3")
+  BTSerial.println("AT+NAMEBARCCS")
+  BTSerial.println("AT+ADDR?")
+  mac_addr = Serial.read()
 }
 
 //
 void loop() {
   //Begin constantly receiving
-
+  receive();
+  
   //Suitable delay
+  delay(50)
 }
 
 //Function to transmit UIK packet when after paired and called
@@ -27,28 +54,53 @@ void collarTransmit() {
 }
 
 //
-void phoneTransmit() {
+void phoneTransmit(int uik) {
+  //Transmit own MAC Address to phone
+  BTSerial.write(mac_addr);
+
   //Loop through list containing received collar UIK's
     //Write and transmit received UIK(s)
-
+  
+  
   //Delete all received UIK's
 }
 
 //Likely will need formula to avoid data collision
 void receive() {
-  //Check to see if data can be read
-    //Read data in size of UIK packet
+  //Counter initialization  
+  static byte count = 10
   
+  //Check to see if data can be read from buffer
+    //Read data in size of UIK packet
+  if(BTSerial.available() >= sizeof(Packet)) {
+    BTSerial.readBytes((byte *) & pkt_rx,sizeof(Packet));
+
+    int id_byte = pkt_rx.a;
+    
     //Check to see if data received is from phone
-      //Store packet device memory as variable to pass to collarTransmit()
-      //so that UIK can be generated
-      //Call phoneTransmit()
+    if(id_byte == phone_id_byte){
+      //Change packet tx struct to UIK sent from phone
+      pkt_tx.a = IDENTIFIER;
+      pkt_tx.b = pkt_rx.b;
+      pkt_tx.c = pkt_rx.c;
+      pkt_tx.d = pkt_tx.d;
+
+      int own_uik = concatenate(pkt_tx.a, pkt_tx.b, pkt_tx.c, pkt_tx.d);
+
+      phoneTransmit();
+    }
+  }
   
     //Check to see if data is UIK of BARCCS device
-      //If so, begin x sec timer before forced disconnect
       //Store UIK packet in device memory (as str in list) only if that UIK
       //is not already stored in list (list comprehension?)
+    if(id_byte == collar_id_byte){
+      //Begin timer
+      chrono.restart()
 
+      //Concatenate received packet
+      //Add packet to array of x length
+    }
     //Flush serial buffer
 
     //Transmit own UIK
@@ -57,6 +109,17 @@ void receive() {
       //Force disconnect with GPIO
 
   //Try transmitting a few times for initiation
+}
+
+int concatenate(int a, int b, int c, int d) {
+  String s1 = String(a);
+  String s2 = String(b);
+  String s3 = String(c);
+  String s4 = String(d);
+
+  String uik = String(s1 + s2 + s3 + s4);
+
+  return uik;
 }
 
 //For debugging
